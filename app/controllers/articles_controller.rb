7b275905ac9a823caf29007ac1484a9c6302ac6d
manan <manansingh20010@gmail.com>
2021-02-23 17:58:51 +0530

    complete implementation of  articles feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,52 @@
-# Articles controller
+# frozen_string_literal: true
 
+# Articles controller
 class ArticlesController < ApplicationController
-  # attr_accessor :article
-  def show
-    @article = Article.find(params[:id])
-  end
+  before_action :set_article, only: %i[show edit update destroy]
+  def show; end
 
   def index
     @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit; end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Article was saved!'
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = 'Article was edited!'
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = 'Article was deleted!'
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
+#Article model
 class Article < ApplicationRecord
-  validates :title, presence: true
-  validates :description, presence: true
+  validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
