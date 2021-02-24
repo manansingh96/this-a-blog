2f185d59d8c60420de10539996d6cedbb3c6d09e
manan <manansingh20010@gmail.com>
2021-02-25 01:51:00 +0530

    added user sessions and login/logout to navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,20 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
+    user = User.find_by(email_id: params[:session][:email_id].downcase)
+    if user&.authenticate(params[:session][:password])
+      session[:user_id]
+      flash[:notice] = 'Logged in successfully'
+      redirect_to user
+    else
+      flash.now[:alert] = 'There was something wrong'
+      render 'new'
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:notice] = "Logged out successfully"
+    redirect_to root_path
   end
 end
