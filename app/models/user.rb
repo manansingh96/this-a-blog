69ef708bff1499c044621dc49e5d95af260296bb
manan <manansingh20010@gmail.com>
2021-02-24 18:53:30 +0530

    added user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@
 
 # User model
 class User < ApplicationRecord
+  validates :username,
+            presence: true,
+            length: { minimum: 3, maximum: 25},
+            uniqueness: { case_sensitive: false }
+  validates :email_id,
+            presence: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP },
+            uniqueness: { case_sensitive: false }
 end
