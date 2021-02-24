5b4fabae52d1eab4ef8c3f47997e42efd2b24ae6
manan <manansingh20010@gmail.com>
2021-02-25 01:15:24 +0530

    added login form

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   resources :users, except: %i[new]
   get '/bloggers', to: 'users#index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
 end
